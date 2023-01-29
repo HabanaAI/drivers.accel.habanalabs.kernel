b7a38a35a2875b238997400acdc47b4e2251a591
Koby Elbaz <kelbaz@habana.ai>
2023-01-29 14:39:52 +0200

    Revert "[SW-108320] habanalabs: enable graceful reset mechanism for compute-reset"
    This reverts commit 9a5b9b2b190242633c0c9f830179bc517ecb761e.
    
    Change-Id: I9872cdd392f3c73595ba1836c4f4faa150dbd717
    Signed-off-by: Koby Elbaz <kelbaz@habana.ai>

--- common/device.c
+++ common/device.c
@@ -867,14 +867,14 @@ static void device_hard_reset_pending(struct work_struct *work)
 
 static void device_release_watchdog_func(struct work_struct *work)
 {
-	struct hl_device_reset_work *watchdog_work =
-			container_of(work, struct hl_device_reset_work, reset_work.work);
-	struct hl_device *hdev = watchdog_work->hdev;
+	struct hl_device_reset_work *device_release_watchdog_work =
+				container_of(work, struct hl_device_reset_work, reset_work.work);
+	struct hl_device *hdev = device_release_watchdog_work->hdev;
 	u32 flags;
 
-	dev_dbg(hdev->dev, "Device wasn't released in time. Initiate hard-reset.\n");
+	dev_dbg(hdev->dev, "Device wasn't released in time. Initiate device reset.\n");
 
-	flags = watchdog_work->flags | HL_DRV_RESET_HARD | HL_DRV_RESET_FROM_WD_THR;
+	flags = device_release_watchdog_work->flags | HL_DRV_RESET_FROM_WD_THR;
 
 	hl_device_reset(hdev, flags);
 }
@@ -1771,17 +1771,15 @@ do_reset:
 
 		/* Cancel the device release watchdog work if required.
 		 * In case of reset-upon-device-release while the release watchdog work is
-		 * scheduled due to a hard-reset, do hard-reset instead of compute-reset.
+		 * scheduled, do hard-reset instead of compute-reset.
 		 */
 		if ((hard_reset || from_dev_release) && hdev->reset_info.watchdog_active) {
-			struct hl_device_reset_work *watchdog_work =
-					&hdev->device_release_watchdog_work;
-
 			hdev->reset_info.watchdog_active = 0;
 			if (!from_watchdog_thread)
-				cancel_delayed_work_sync(&watchdog_work->reset_work);
+				cancel_delayed_work_sync(
+						&hdev->device_release_watchdog_work.reset_work);
 
-			if (from_dev_release && (watchdog_work->flags & HL_DRV_RESET_HARD)) {
+			if (from_dev_release) {
 				hdev->reset_info.in_compute_reset = 0;
 				flags |= HL_DRV_RESET_HARD;
 				flags &= ~HL_DRV_RESET_DEV_RELEASE;
@@ -2122,6 +2120,10 @@ int hl_device_cond_reset(struct hl_device *hdev, u32 flags, u64 event_mask)
 {
 	struct hl_ctx *ctx = NULL;
 
+	/* Device release watchdog is only for hard reset */
+	if (!(flags & HL_DRV_RESET_HARD) && hdev->asic_prop.allow_inference_soft_reset)
+		goto device_reset;
+
 	/* F/W reset cannot be postponed */
 	if (flags & HL_DRV_RESET_BYPASS_REQ_TO_FW)
 		goto device_reset;
@@ -2149,7 +2151,7 @@ int hl_device_cond_reset(struct hl_device *hdev, u32 flags, u64 event_mask)
 		goto out;
 
 	hdev->device_release_watchdog_work.flags = flags;
-	dev_dbg(hdev->dev, "Device is going to be hard-reset in %u sec unless being released\n",
+	dev_dbg(hdev->dev, "Device is going to be reset in %u sec unless being released\n",
 		hdev->device_release_watchdog_timeout_sec);
 	schedule_delayed_work(&hdev->device_release_watchdog_work.reset_work,
 				msecs_to_jiffies(hdev->device_release_watchdog_timeout_sec * 1000));
