b9e2ca485ebe64dd15ac00a02514f063ed949a1c
farah kassabri <fkassabri@habana.ai>
2023-02-02 15:10:35 +0200

    [SW-77960] gaudi3: use P-DUP engines for MMU prepare
    Upon each context init driver passes over all engines in the system
    and sets the MMU bypass and asid for them.
    this flow is done by write through the PCIe bar to configure
    all of those regs one by one.
    this patch will use the duplication engine for this matter
    which will improve performance.
    
    Change-Id: Ida6b0b918e58a2ae656ea4b2708ddf63adc0b5f9
    Signed-off-by: farah kassabri <fkassabri@habana.ai>

--- gaudi3/gaudi3_axuser.c
+++ gaudi3/gaudi3_axuser.c
@@ -10,21 +10,14 @@
 #include "gaudi3P.h"
 #include "../common/habanalabs.h"
 #include "../include/gaudi3/asic_reg/gaudi3_regs.h"
-
-#define GAUDI3_HB_RW_MMU_BYPASS_OVRD \
-		(PDMA_CH_B_AXUSER_HBW_HB_MMU_BYPASS_OVRD_WR_M | \
-			PDMA_CH_B_AXUSER_HBW_HB_MMU_BYPASS_OVRD_RD_M)
-
-#define GAUDI3_HB_RW_ASID_OVRD \
-			(PDMA_CH_B_AXUSER_HBW_HB_ASID_OVRD_WR_M | \
-				PDMA_CH_B_AXUSER_HBW_HB_ASID_OVRD_RD_M)
+#include "gaudi3_masks.h"
 
 void gaudi3_axuser_hbw_mmu_bp_set(struct hl_device *hdev, u32 axuser_hbw_reg_base, bool bypass)
 {
-	u32 rw_mmu_bp = bypass ? GAUDI3_HB_RW_MMU_BYPASS_OVRD : 0;
+	u32 rw_mmu_bp = bypass ? HBW_RW_MMU_BYPASS_OVRD : 0;
 
 	WREG32(axuser_hbw_reg_base + mmPDMA_CH_B_AXUSER_HBW_HB_MMU_BYPASS_OVRD,
-			GAUDI3_HB_RW_MMU_BYPASS_OVRD);
+			HBW_RW_MMU_BYPASS_OVRD);
 	WREG32(axuser_hbw_reg_base + mmPDMA_CH_B_AXUSER_HBW_HB_MMU_BYPASS, rw_mmu_bp);
 }
 
@@ -39,7 +32,7 @@ void gaudi3_axuser_hbw_asid_set(struct hl_device *hdev, u32 axuser_hbw_reg_base,
 	u32 rw_asid = FIELD_PREP(PDMA_CH_B_AXUSER_HBW_HB_ASID_RD_M, asid) |
 			FIELD_PREP(PDMA_CH_B_AXUSER_HBW_HB_ASID_WR_M, asid);
 
-	WREG32(axuser_hbw_reg_base + mmPDMA_CH_B_AXUSER_HBW_HB_ASID_OVRD, GAUDI3_HB_RW_ASID_OVRD);
+	WREG32(axuser_hbw_reg_base + mmPDMA_CH_B_AXUSER_HBW_HB_ASID_OVRD, HBW_RW_ASID_OVRD);
 	WREG32(axuser_hbw_reg_base + mmPDMA_CH_B_AXUSER_HBW_HB_ASID, rw_asid);
 }
 
