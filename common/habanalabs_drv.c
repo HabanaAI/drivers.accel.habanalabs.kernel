000426c2bcce53d21a86a018139cfd2a79d50e22
Ofir Bitton <obitton@habana.ai>
2023-02-15 12:53:21 +0200

    [SW-0] gaudi3: remove defaults for gaudi3 single-die asic
    There will be no such product, no need for this.
    
    Change-Id: Ia4d611f6456b384dda37cc6634ad234f64789e43
    Signed-off-by: Ofir Bitton <obitton@habana.ai>

--- common/habanalabs_drv.c
+++ common/habanalabs_drv.c
@@ -1299,34 +1299,6 @@ static void set_driver_behavior_per_device(struct hl_device *hdev)
 		hdev->hbm_compression_enable = 1;
 		break;
 
-	case ASIC_GAUDI3_SINGLE_DIE:
-		hdev->dram_enable = 0;
-		hdev->fw_components = FW_TYPE_PREBOOT_CPU;
-		hdev->mmu_enable = 0;
-		hdev->security_enable = 0;
-		hdev->tpc_mask = 0;
-		hdev->mme_mask = 0;
-		hdev->pdma_ch_mask = 0xFFF;
-		hdev->edma_mask = 0x0;
-		hdev->hard_reset_on_fw_events = 0;
-		hdev->decoder_mask = 0;
-		hdev->dram_binning = 0x0;
-		hdev->edma_binning = 0x0;
-		hdev->tpc_binning = 0x0;
-		hdev->decoder_binning = 0x0;
-		hdev->scrub_arc_dccm = 0;
-		hdev->axi_drain = AXI_DRAIN_SKIP;
-		hdev->fw_communication_enable = 0;
-		hdev->sched_arc_mask = 0xFF;
-		hdev->rotator_mask = 0x0;
-		hdev->use_8_bit_hops = 0;
-		hdev->priv_security_enable = 0;
-		hdev->cache_enable = 1;
-		hdev->ptw_bypass_enable = 1;
-		hdev->rotator_binning = 0;
-		hdev->hbm_compression_enable = 1;
-		break;
-
 	case ASIC_GAUDI3_SIM:
 		hdev->dram_enable = 1;
 		hdev->fw_components = 0;
