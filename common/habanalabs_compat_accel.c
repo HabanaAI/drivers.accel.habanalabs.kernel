9c3d705d80516a52a81de507d4484d98f6b86405
Tomer Tayar <ttayar@habana.ai>
2023-04-02 13:00:13 +0300

    [SW-114047] compat: put accel sysfs attributes under a device subdirectory
    When registering to the accel subsystem, the sysfs attribute files are
    created under "/sys/class/accel/accel<n>/device".
    However when creating a "fake" accel device, as there is no parent
    device, the sysfs attributes files will be created directly under
    "/sys/class/accel/accel<n>".
    To make it similar to "real" accel device, add a "device" name to the
    sysfs attribute groups, which will lead to creating a subdirectory with
    this name.
    
    As having the same name for several attribute groups will fail due to
    trying to create duplicate filenames, combine the attributes from all
    groups into a single attribute group.
    
    Change-Id: I99c33f80a9c8ccd34770dbd9589fcfafe2f201bc
    Signed-off-by: Tomer Tayar <ttayar@habana.ai>

--- common/habanalabs_compat_accel.c
+++ common/habanalabs_compat_accel.c
@@ -7,7 +7,9 @@
  */
 
 #include <linux/debugfs.h>
+#include <linux/slab.h>
 
+#include "habanalabs_compat.h"
 #include "habanalabs_compat_accel.h"
 
 #define ACCEL_MAX_MINORS	256
@@ -90,4 +92,84 @@ int __init hl_accel_init(void)
 	accel_debugfs_root = debugfs_create_dir("accel", NULL);
 
 	return 0;
+}
+
+static int accel_device_create_combined_group(struct device *dev,
+						struct attribute_group *combined_group,
+						const struct attribute_group **groups)
+{
+	u32 i, j, num_attrs = 0, num_bin_attrs = 0, attrs_idx = 0, bin_attrs_idx = 0;
+	struct bin_attribute **bin_attrs;
+	struct attribute **attrs;
+
+	for (i = 0 ; groups[i] ; ++i) {
+		for (j = 0 ; groups[i]->attrs && groups[i]->attrs[j] ; ++j)
+			++num_attrs;
+		for (j = 0 ; groups[i]->bin_attrs && groups[i]->bin_attrs[j] ; ++j)
+			++num_bin_attrs;
+	}
+
+	/* add 1 for the NULL at the end of the combined attribute arrays */
+	++num_attrs;
+	++num_bin_attrs;
+
+	attrs = kmalloc_array(num_attrs, sizeof(*combined_group->attrs), GFP_KERNEL | __GFP_ZERO);
+	if (!attrs)
+		return -ENOMEM;
+
+	bin_attrs = kmalloc_array(num_bin_attrs, sizeof(*combined_group->bin_attrs),
+					GFP_KERNEL | __GFP_ZERO);
+	if (!bin_attrs) {
+		kfree(attrs);
+		return -ENOMEM;
+	}
+
+	for (i = 0 ; groups[i] ; ++i) {
+		for (j = 0 ; groups[i]->attrs && groups[i]->attrs[j] ; ++j)
+			attrs[attrs_idx++] = groups[i]->attrs[j];
+		for (j = 0 ; groups[i]->bin_attrs && groups[i]->bin_attrs[j] ; ++j)
+			bin_attrs[bin_attrs_idx++] = groups[i]->bin_attrs[j];
+	}
+
+	combined_group->attrs = attrs;
+	combined_group->bin_attrs = bin_attrs;
+
+	return 0;
+}
+
+static void accel_device_destroy_combined_group(struct device *dev,
+						struct attribute_group *combined_group)
+{
+	kfree(combined_group->attrs);
+	kfree(combined_group->bin_attrs);
+}
+
+int hl_accel_device_add_groups(struct device *dev, const struct attribute_group **groups)
+{
+	struct attribute_group combined_group = {.name = "device"};
+	int rc;
+
+	rc = accel_device_create_combined_group(dev, &combined_group, groups);
+	if (rc)
+		return rc;
+
+	rc = device_add_group(dev, &combined_group);
+
+	accel_device_destroy_combined_group(dev, &combined_group);
+
+	return rc;
+}
+
+void hl_accel_device_remove_groups(struct device *dev, const struct attribute_group **groups)
+{
+	struct attribute_group combined_group = {.name = "device"};
+	int rc;
+
+	rc = accel_device_create_combined_group(dev, &combined_group, groups);
+	if (rc)
+		return;
+
+	device_remove_group(dev, &combined_group);
+
+	accel_device_destroy_combined_group(dev, &combined_group);
 }
