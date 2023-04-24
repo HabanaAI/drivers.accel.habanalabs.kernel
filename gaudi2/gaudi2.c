fa8b9fea0baa299bf499793ec8f45b024c8a2324
Ofir Bitton <obitton@habana.ai>
2023-04-24 14:17:03 +0300

    [SW-142252] accel/habanalabs: always fetch pci addr_dec error info
    Due to missing indication of address decode source (LBW/HBW),
    we should always try and fetch extended information.
    
    Change-Id: I488c1e59a9659f274f9d6d0b925e346d133fb1b1
    Signed-off-by: Ofir Bitton <obitton@habana.ai>

--- gaudi2/gaudi2.c
+++ gaudi2/gaudi2.c
@@ -9745,14 +9745,21 @@ static int gaudi2_print_pcie_addr_dec_info(struct hl_device *hdev, u16 event_typ
 			"err cause: %s", gaudi2_pcie_addr_dec_error_cause[i]);
 		error_count++;
 
-		switch (intr_cause_data & BIT_ULL(i)) {
-		case PCIE_WRAP_PCIE_IC_SEI_INTR_IND_AXI_LBW_ERR_INTR_MASK:
-			hl_check_for_glbl_errors(hdev);
-			break;
-		case PCIE_WRAP_PCIE_IC_SEI_INTR_IND_BAD_ACCESS_INTR_MASK:
-			gaudi2_print_pcie_mstr_rr_mstr_if_razwi_info(hdev, event_mask);
-			break;
-		}
+		/*
+		 * TODO: enable once SW-142252 is resolved, meanwhile always check for LBW and HBW
+		 * additional info as the indication itself is sometimes missing
+		 *
+		 * switch (intr_cause_data & BIT_ULL(i)) {
+		 * case PCIE_WRAP_PCIE_IC_SEI_INTR_IND_AXI_LBW_ERR_INTR_MASK:
+		 *      hl_check_for_glbl_errors(hdev);
+		 *      break;
+		 * case PCIE_WRAP_PCIE_IC_SEI_INTR_IND_BAD_ACCESS_INTR_MASK:
+		 *      gaudi2_print_pcie_mstr_rr_mstr_if_razwi_info(hdev, event_mask);
+		 *      break;
+		 * }
+		 */
+		hl_check_for_glbl_errors(hdev);
+		gaudi2_print_pcie_mstr_rr_mstr_if_razwi_info(hdev, event_mask);
 	}
 
 	return error_count;
