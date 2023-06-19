994a6f65d479ea36e9101044b3f0eb4ddfaa7eab
Bharat Jauhari <bjauhari@habana.ai>
2023-06-19 11:02:23 +0300

    [SW-146391] gaudi2: add debug info for engine idle status
    Adding info level trace to track NIC engine idle status during CI runs
    in context of SW-146391. This is a temporary patch and not be
    up-streamed.
    
    Signed-off-by: Bharat Jauhari <bjauhari@habana.ai>
    Change-Id: I43043818a4ddfa4d635b1f9be5d1c140bc84ffbb

--- gaudi2/gaudi2.c
+++ gaudi2/gaudi2.c
@@ -7661,6 +7661,11 @@ static bool gaudi2_get_nic_idle_status(struct hl_device *hdev, u64 *mask_arr, u8
 		if (mask && !is_eng_idle)
 			set_bit(engine_idx, mask);
 
+		/* SW-146391: For debug, not for upstream. */
+		if (!e && !is_eng_idle)
+			dev_info(hdev->dev, "NIC%u qm_glbl_sts0:0x%x qm_cgm_sts:0x%x\n",
+					i, qm_glbl_sts0, qm_cgm_sts);
+
 		if (e)
 			hl_engine_data_sprintf(e, nic_fmt, i, is_eng_idle ? "Y" : "N",
 						qm_glbl_sts0, qm_cgm_sts);
