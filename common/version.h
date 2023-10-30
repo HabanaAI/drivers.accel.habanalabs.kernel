166aebae672d8eb603f95b8a69d6229e77149664
Ido Gan <igan@habana.ai>
2023-10-30 13:36:13 +0000

    [SW-0] Promote to 1.14.0
    Change-Id: I30bd1147cb8e832ac6845ab1ad94fbcf770326b3
    Signed-off-by: Ido Gan <igan@habana.ai>

--- common/version.h
+++ common/version.h
@@ -8,9 +8,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE          "20230831"
+#define HL_DRIVER_DATE          "20231030"
 #define HL_DRIVER_MAJOR         1
-#define HL_DRIVER_MINOR         13
+#define HL_DRIVER_MINOR         14
 #define HL_DRIVER_PATCHLEVEL    0
 
 #endif /* HL_VERSION_H */
