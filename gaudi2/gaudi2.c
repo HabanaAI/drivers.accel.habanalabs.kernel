a7ecbc4ca7f55f028f132939b5de5416a58a4f69
Yuval Elmaliah <yelmaliah@habana.ai>
2023-12-31 14:26:52 +0200

    [SW-170017] accel/habanalabs/gaudi2: clear affinity hint
    Clear affinity hint before calling free_irq.
    
    Change-Id: Id5b9424c2f7328a777eda9b7f4ff4dee99b86ba3
    Signed-off-by: Yuval Elmaliah <yelmaliah@habana.ai>

--- gaudi2/gaudi2.c
+++ gaudi2/gaudi2.c
@@ -4735,6 +4735,7 @@ free_user_irq:
 			i < GAUDI2_IRQ_NUM_USER_FIRST + user_irq_init_cnt ; i++, j++) {
 
 		irq = pci_irq_vector(hdev->pdev, i);
+		irq_set_affinity_hint(irq, NULL);
 		free_irq(irq, &hdev->user_interrupt[j]);
 	}
 	irq = pci_irq_vector(hdev->pdev, GAUDI2_IRQ_NUM_UNEXPECTED_ERROR);
@@ -4815,6 +4816,7 @@ static void gaudi2_disable_msix(struct hl_device *hdev)
 			k < hdev->asic_prop.user_interrupt_count ; i++, j++, k++) {
 
 		irq = pci_irq_vector(hdev->pdev, i);
+		irq_set_affinity_hint(irq, NULL);
 		free_irq(irq, &hdev->user_interrupt[j]);
 	}
 
