e96949f6501efeee06e09d97bb4655f3053a4dda
Tomer Tayar <ttayar@habana.ai>
2024-02-15 17:42:19 +0200

    [SW-169793] accel/habanalabs/gaudi3: enable reset upon FW events
    No FW events are expected now after boot or reset, so the reset upon FW
    events can be enabled.
    
    Change-Id: I3de433a7bd90d5392568db02204c68b1a66bcaaa
    Signed-off-by: Tomer Tayar <ttayar@habana.ai>

--- gaudi3/gaudi3.c
+++ gaudi3/gaudi3.c
@@ -13700,10 +13700,7 @@ static void gaudi3_set_reset_flags_and_event_mask(struct hl_device *hdev,
 		compute_reset = true;
 		break;
 	case INT_COMP_TYPE_NIC:
-		/* NIC SPI events are not errors.
-		 * As to the PLDM exception, see H9-5611
-		 */
-		/* TODO - SW-169793: Validate that this doesn't happen on ASIC */
+		/* NIC SPI events are not errors. As to the PLDM exception, see H9-5611. */
 		if (agg_grp_type == INT_GRP_TYPE_SEI && !hdev->pldm) {
 			*event_mask = HL_NOTIFIER_EVENT_USER_ENGINE_ERR;
 			hard_reset = true;
