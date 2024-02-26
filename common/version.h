4b2b71f63fb29d4ab919095fbf751c2d00701b59
Saar Gamzo <sgamzo@habana.ai>
2024-02-26 14:01:31 +0000

    [SW-0] Promote to 1.16.0
    Change-Id: I7eddbf8e5accd9b1e94f5b79c4441fd930ff6a7c
    Signed-off-by: Saar Gamzo <sgamzo@habana.ai>

--- common/version.h
+++ common/version.h
@@ -8,9 +8,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE          "20240104"
+#define HL_DRIVER_DATE          "20240226"
 #define HL_DRIVER_MAJOR         1
-#define HL_DRIVER_MINOR         15
+#define HL_DRIVER_MINOR         16
 #define HL_DRIVER_PATCHLEVEL    0
 
 #endif /* HL_VERSION_H */
