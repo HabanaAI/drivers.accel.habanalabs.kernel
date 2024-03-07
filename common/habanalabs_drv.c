4ca1d6f5bf5a9075150bf54fce9545e26754db8d
Tomer Tayar <ttayar@habana.ai>
2024-03-07 14:34:35 +0200

    [SW-175981] accel/habanalabs/gaudi3: temporarily disable MME/TPC regulators
    Disable MME/TPC regulators by default due to performance issues.
    Should be removed when NoC configuration is updated.
    
    Change-Id: Ic797a3a2a2b635fb9ca0a2e9a085d2c27e36fb5a
    Signed-off-by: Tomer Tayar <ttayar@habana.ai>

--- common/habanalabs_drv.c
+++ common/habanalabs_drv.c
@@ -1091,6 +1091,7 @@ static void set_driver_behavior_per_device(struct hl_device *hdev)
 		hdev->ptw_bypass_enable = 1;
 		hdev->rotator_binning = 0;
 		hdev->hbm_compression_enable = 0;
+		hdev->disable_h9_regulartors = 1;
 		break;
 
 	case ASIC_GAUDI3_SIM:
