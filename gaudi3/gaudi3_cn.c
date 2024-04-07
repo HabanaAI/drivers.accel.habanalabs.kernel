1d2b471d9c6ec000bded7d8cf53d49150e2c92e1
David Meriin <dmeriin@habana.ai>
2024-04-08 01:08:45 +0300

    [FSW-4677] accel/habanalabs/gaudi3: move common aux functions to gaudi3 aux
    The function pointers below are to be ported from common aux functions to
    ASIC specific aux. Therefore, create the same function pointers in gaudi3
    aux.
    
    Function pointers:
    - spmu_get_stats_info
    - spmu_config
    - spmu_sample
    - poll_reg
    - send_cpu_message
    - set_priv_assertions
    - post_send_status
    - ports_stop_prepare
    - send_port_cpucp_status
    - get_port_statistics
    
    Change-Id: I68c8384037326f1f2fa7e9985bb43e7afabf169c
    Signed-off-by: David Meriin <dmeriin@habana.ai>

--- gaudi3/gaudi3_cn.c
+++ gaudi3/gaudi3_cn.c
@@ -570,6 +570,13 @@ static void gaudi3_cn_set_cn_data(struct hl_device *hdev)
 	gaudi3_aux_ops->dma_pool_free = gaudi3_cn_dma_pool_free;
 	gaudi3_aux_ops->get_hw_block_handle = gaudi3_cn_get_hw_block_handle;
 	gaudi3_aux_ops->user_mmap = gaudi3_cn_user_mmap;
+	gaudi3_aux_ops->spmu_get_stats_info = hl_cn_spmu_get_stats_info;
+	gaudi3_aux_ops->spmu_config = hl_cn_spmu_config;
+	gaudi3_aux_ops->spmu_sample = hl_cn_spmu_sample;
+	gaudi3_aux_ops->set_priv_assertions = hl_cn_set_priv_assertions;
+	gaudi3_aux_ops->poll_reg = hl_cn_poll_reg;
+	gaudi3_aux_ops->send_cpu_message = hl_cn_send_cpu_message;
+	gaudi3_aux_ops->post_send_status = hl_cn_post_send_status;
 }
 
 /**
