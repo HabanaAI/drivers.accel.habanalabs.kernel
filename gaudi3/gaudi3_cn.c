84128dd5833c3a387837c927cb7210f5f84f0af4
David Meriin <dmeriin@habana.ai>
2024-04-26 02:07:36 +0300

    [FSW-4677] accel/habanalabs/gaudi3: call legacy aux funcs from asic specific
    As part of the CN aux interface alignment, call the functions below from
    asic specific functions to asic specific aux functions.
    
    port functions:
    - ports_stop_prepare
    - send_port_cpucp_status
    - get_port_statistics
    
    Change-Id: Ifb6e197e295870af8ab0321906b0a4c5e3219a64
    Signed-off-by: David Meriin <dmeriin@habana.ai>

--- gaudi3/gaudi3_cn.c
+++ gaudi3/gaudi3_cn.c
@@ -707,11 +707,54 @@ static void gaudi3_cn_post_send_status(struct hl_device *hdev, u32 port)
 	/* FW does not mask MSG interrupts, so unmask_irq is not needed */
 }
 
+static void gaudi3_cn_ports_stop_prepare(struct hl_device *hdev, bool fw_reset, bool in_teardown)
+{
+	struct gaudi3_device *gaudi = hdev->asic_specific;
+	struct gaudi3_cn_aux_ops *gaudi3_aux_ops;
+	struct hl_cn *cn = &hdev->cn;
+	struct hbl_aux_dev *aux_dev;
+
+	aux_dev = &cn->cn_aux_dev;
+	gaudi3_aux_ops = &gaudi->cn_aux_ops;
+
+	gaudi3_aux_ops->ports_stop_prepare(aux_dev, fw_reset, in_teardown);
+}
+
+static int gaudi3_cn_send_port_cpucp_status(struct hl_device *hdev, u32 port, u8 cmd, u8 period)
+{
+	struct gaudi3_device *gaudi = hdev->asic_specific;
+	struct gaudi3_cn_aux_ops *gaudi3_aux_ops;
+	struct hl_cn *cn = &hdev->cn;
+	struct hbl_aux_dev *aux_dev;
+
+	aux_dev = &cn->cn_aux_dev;
+	gaudi3_aux_ops = &gaudi->cn_aux_ops;
+
+	return gaudi3_aux_ops->send_port_cpucp_status(aux_dev, port, cmd, period);
+}
+
+static int gaudi3_cn_get_port_statistics(struct hl_device *hdev, u32 port,
+						struct hbl_cn_port_statistics *out)
+{
+	struct gaudi3_device *gaudi = hdev->asic_specific;
+	struct gaudi3_cn_aux_ops *gaudi3_aux_ops;
+	struct hl_cn *cn = &hdev->cn;
+	struct hbl_aux_dev *aux_dev;
+
+	aux_dev = &cn->cn_aux_dev;
+	gaudi3_aux_ops = &gaudi->cn_aux_ops;
+
+	return gaudi3_aux_ops->get_port_statistics(aux_dev, port, out);
+}
+
 static struct hl_cn_port_funcs gaudi3_cn_port_funcs = {
 	.spmu_get_stats_info = gaudi3_cn_spmu_get_stats_info,
 	.spmu_config = gaudi3_cn_spmu_config,
 	.spmu_sample = gaudi3_cn_spmu_sample,
 	.post_send_status = gaudi3_cn_post_send_status,
+	.ports_stop_prepare = gaudi3_cn_ports_stop_prepare,
+	.send_port_cpucp_status = gaudi3_cn_send_port_cpucp_status,
+	.get_port_statistics = gaudi3_cn_get_port_statistics,
 };
 
 struct hl_cn_funcs gaudi3_cn_funcs = {
