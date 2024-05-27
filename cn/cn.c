6b28c37214f3deffe4cf6ca09d44906f7b5221c7
David Meriin <dmeriin@habana.ai>
2024-05-27 14:04:21 +0300

    [FSW-4677] accel/habanalabs/cn: add lanes_per_port field get_ports_info()
    lanes_per_port should be part of the CN aux function get_ports_info().
    
    Change-Id: I43e3cb48c0fe5d894f977959502eca84b3693cc7
    Signed-off-by: David Meriin <dmeriin@habana.ai>

--- cn/cn.c
+++ cn/cn.c
@@ -436,6 +436,7 @@ static void hl_cn_get_ports_info(struct hbl_aux_dev *aux_dev,
 	hbl_cn_aux_ports_info->ports_pol_rx_mask = cn_cpucp_info->pol_rx_mask[0];
 	hbl_cn_aux_ports_info->ports_ext_mask = cn_cpucp_info->link_ext_mask[0];
 	hbl_cn_aux_ports_info->ports_auto_neg_mask = cn_cpucp_info->auto_neg_mask[0];
+	hbl_cn_aux_ports_info->lanes_per_port = hdev->cn.lanes_per_port;
 }
 
 static void hl_cn_cpucp_info_le_to_cpu(struct cpucp_nic_info *cpucp_nic_info,
