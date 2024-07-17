2d025f34e67c9ca165dc096cafc04129361183ed
Didi Freiman <dfreiman@habana.ai>
2024-07-17 11:24:24 +0300

    [SW-190655] accel/habanalabs: disable device access after CPLD_SHUTDOWN
    After cpld shutdown event the device is not usable anymore.
    
    Signed-off-by: Didi Freiman <dfreiman@habana.ai>
    Change-Id: Ia6a481b1bc519dc6352f55db88f3d7bf3870eb3a

--- common/habanalabs.h
+++ common/habanalabs.h
@@ -3830,6 +3830,7 @@ struct eq_heartbeat_debug_info {
  *                    addresses.
  * @is_in_dram_scrub: true if dram scrub operation is on going.
  * @disabled: is device disabled.
+ * @cpld_shutdown: is cpld shutdown.
  * @late_init_done: is late init stage was done during initialization.
  * @hwmon_initialized: is H/W monitor sensors was initialized.
  * @reset_on_lockup: true if a reset should be done in case of stuck CS, false
@@ -4037,6 +4038,7 @@ struct hl_device {
 	u16				cpu_pci_msb_addr;
 	u8				is_in_dram_scrub;
 	u8				disabled;
+	u8				cpld_shutdown;
 	u8				late_init_done;
 	u8				hwmon_initialized;
 	u8				reset_on_lockup;
@@ -4742,6 +4744,7 @@ int map_device_va_from_dmabuf_fd(struct hl_ctx *ctx, struct hl_mem_in *args,
 				u64 *device_addr);
 void hl_eq_heartbeat_event_handle(struct hl_device *hdev);
 void hl_handle_clk_change_event(struct hl_device *hdev, u16 event_type, u64 *event_mask);
+void hl_eq_cpld_shutdown_event_handle(struct hl_device *hdev, u16 event_id, u64 *event_mask);
 
 #ifdef CONFIG_DEBUG_FS
 
