840d562713bc609f4c6b8e42ae2cab317e22fedb
David Meriin <dmeriin@habana.ai>
2024-08-23 23:29:42 +0300

    [FSW-4679] accel/habanalabs/cn: add cmd_control to hl_cn_funcs
    cmd_control will be implemented per asic.
    
    Change-Id: I6ca66874f7cedce4d00123c3871ca6a1192c2d5c
    Signed-off-by: David Meriin <dmeriin@habana.ai>

--- cn/cn.h
+++ cn/cn.h
@@ -140,6 +140,7 @@ struct hl_cn_port_funcs {
  * @set_hw_cap: set HW capability (on/off).
  * @set_cn_data: ASIC data to be used by the CN driver.
  * @mmap: Map CN memory.
+ * @cmd_control: command control IOCTL.
  * @port_funcs: functions called from common code for a specific NIC port.
  */
 struct hl_cn_funcs {
@@ -148,6 +149,7 @@ struct hl_cn_funcs {
 	void (*set_hw_cap)(struct hl_device *hdev, bool enable);
 	void (*set_cn_data)(struct hl_device *hdev);
 	int (*mmap)(struct hl_device *hdev, u32 asid, struct vm_area_struct *vma);
+	int (*cmd_control)(struct hl_device *hdev, u32 op, void *input, void *output, u32 asid);
 	struct hl_cn_port_funcs *port_funcs;
 };
 
