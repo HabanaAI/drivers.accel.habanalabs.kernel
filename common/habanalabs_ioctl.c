da1e0e2c65eab65e1ad7ce7edbbb5d519cb41910
ariel aviad <aaviad@habana.ai>
2024-11-07 11:16:48 +0200

    [SW-197311] cpucp: add type to get supported states
    add type HL_GET_SUPPORTED_P_STATES to geniric ioctl to
    get supported performance states
    
    Change-Id: I51167e27a8d96b2b6ba9e945ed34b44ccd79c5a5
    Signed-off-by: ariel aviad <aaviad@habana.ai>

--- common/habanalabs_ioctl.c
+++ common/habanalabs_ioctl.c
@@ -1286,6 +1286,9 @@ static int send_fw_generic_request(struct hl_device *hdev, struct hl_info_args *
 	case HL_GET_P_STATE:
 		need_input_buff = false;
 		break;
+	case HL_GET_SUPPORTED_P_STATES:
+		need_input_buff = false;
+		break;
 	default:
 		return -EINVAL;
 	}
