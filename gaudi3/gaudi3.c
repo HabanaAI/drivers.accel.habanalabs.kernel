d298cb5673c9f932042d3d2b4788e60b691f9449
Farah Kassabri <fkassabri@habana.ai>
2024-12-08 11:05:30 +0200

    [SW-211882] accel/habanalabs/gaudi3: support graceful FW reset
    Add support for graceful FW reset message.
    
    Signed-off-by: Farah Kassabri <fkassabri@habana.ai>

--- gaudi3/gaudi3.c
+++ gaudi3/gaudi3.c
@@ -13648,6 +13648,9 @@ static int gaudi3_handle_msg_event(struct hl_device *hdev,
 	case EQ_EVENT_VR_OC_VIOLATION:
 		dev_warn(hdev->dev, "VR over current violation event\n");
 		break;
+	case EQ_EVENT_FW_RESET:
+		dev_warn(hdev->dev, "FW reset event\n");
+		break;
 	default:
 		dev_err(hdev->dev, "undefined msg event %d received\n", event_type);
 		rc = -EINVAL;
