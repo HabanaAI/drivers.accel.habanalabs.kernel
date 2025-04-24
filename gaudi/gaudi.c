961a6b6264ecac86ffddbae3801c87608465248a
Ariel Suller <asuller@habana.ai>
2025-04-24 14:52:45 +0300

    [SW-227088] accel/habanalabs/gaudi: verify preboot is in valid state at init
    W/A for cases where at host reboot PRSTN signal is
    not sent to device, hence only the host is being reset.
    In such cases, when the host tries to load the driver
    it fails on polling for preboot, since it has already
    done.
    
    This commit adds a verification at Gaudi's early init that
    preboot boot process is done.
    This commit should be reverted once fw implements
    this solution.
    
    Signed-off-by: Ariel Suller <asuller@habana.ai>

--- gaudi/gaudi.c
+++ gaudi/gaudi.c
@@ -888,6 +888,24 @@ pci_init:
 	if (rc)
 		goto free_queue_props;
 
+	/*
+	 * W/A for cases where host server does not send reboot to device, causing a corner case
+	 * where the device is not in a valid state after a reboot.
+	 * In such case, the device needs to be reset before initialization
+	 * TODO: should be removed once [SW-227103] is done
+	 */
+	rc = hl_fw_verify_preboot_boot_status(hdev);
+	if (rc) {
+		hl_err(hdev, "preboot status verification failed, going to reset device\n");
+		gaudi_fw_security_emulation_fini(hdev, true);
+		rc = hdev->asic_funcs->hw_fini(hdev, true, false);
+		if (rc) {
+			hl_err(hdev,
+			       "failed to reset HW after preboot status verification failure\n");
+			goto pci_fini;
+		}
+	}
+
 	/* Before continuing in the initialization, we need to read the preboot
 	 * version to determine whether we run with a security-enabled firmware
 	 */
