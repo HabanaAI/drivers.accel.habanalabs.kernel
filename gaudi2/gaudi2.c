fb2197da20d77b2c21cf09684cab1336a1c3ebec
Ariel Suller <asuller@habana.ai>
2025-06-15 10:45:59 +0300

    [SW-232122] accel/habanalabs/gaudi2: change preboot verification print to dbg
    the print done when doing reset for device
    that is in prebbot stage when driver is loading
    (as part of a W/A) should be a dbg print and
    not error.
    
    Signed-off-by: Ariel Suller <asuller@habana.ai>
    
    Signed-off-by: Ariel Suller <asuller@habana.ai>

--- gaudi2/gaudi2.c
+++ gaudi2/gaudi2.c
@@ -3608,7 +3608,7 @@ static int gaudi2_early_init(struct hl_device *hdev)
 	 */
 	rc = hl_fw_verify_preboot_boot_status(hdev);
 	if (rc) {
-		hl_err(hdev, "preboot status verification failed, going to reset device\n");
+		hl_dbg(hdev, "preboot status verification failed, going to reset device\n");
 		/* gic_host_halt_irq is the register to which a fw reset request should be sent
 		 * in secured fw case (since driver can't reset by itself). This register's address
 		 * is derrived from fw, but at this point we don't have the fw yet, so we need to
