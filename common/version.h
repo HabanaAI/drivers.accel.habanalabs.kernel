f978aa18be09381eec3ad237880a9ba7ac516370
Habana Devops Branch Out User <sys_HLBranchOutUser@intel.com>
2026-03-05 20:30:56 +0200

    Update drivers/accel/habanalabs/common/version.h for branch master-branchout

--- common/version.h
+++ common/version.h
@@ -8,9 +8,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE          "20250915"
+#define HL_DRIVER_DATE          "20260305"
 #define HL_DRIVER_MAJOR         1
 #define HL_DRIVER_MINOR         24
-#define HL_DRIVER_PATCHLEVEL    0
+#define HL_DRIVER_PATCHLEVEL    1
 
 #endif /* HL_VERSION_H */
